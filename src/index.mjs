08db63597dae0084918fd6b0505450b5676eabec
Kevin Koech <kevinkoech@proton.me>
2024-04-06 13:58:51 +0300

    [Chore]: Set up API Swagger Docs in /api-docs route

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swagger from './swagger.mjs';
 
 // Load environment variables
-// If not existant, use 3300
+// If not existent, use port 3300
 dotenv.config();
 const PORT = process.env.PORT || 3300;
 
+// Load Express
 const app = express();
+
+// Generate API documentation
+swagger(app);
 
 // Start server on specified port
 app.listen(PORT, () => {
