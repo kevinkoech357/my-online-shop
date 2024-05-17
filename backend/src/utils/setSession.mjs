07500a847b586ef2f46de98cbefe980ffdea07b0
Kevin Koech <kevinkoech@proton.me>
2024-05-17 11:08:43 +0300

    [Update]: Add a middleware that checks if users session is valid Set a user session after successful credentials validation before logging in

---
+++ backend/src/utils/setSession.mjs
@@ -0,0 +1,32 @@
+import { randomBytesAsync } from './hashData.mjs';
+
+// Define set session function that creates and saves a user's session during login
+const setSessionOnLogin = async (user, req, res) => {
+  try {
+    // Generate a unique session ID
+    const buffer = await randomBytesAsync(16);
+    const sessionId = buffer.toString('hex');
+
+    // Set the cookie
+    const cookieName = 'userSession';
+    const cookieValue = sessionId;
+
+    // Set maxAge to 72 hours
+    const maxAge = 60000 * 60 * 72;
+
+    // Set session variables
+    req.session.user = {
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email
+    };
+
+    // Set the session cookie
+    res.cookie(cookieName, cookieValue, { maxAge, signed: true });
+  } catch (error) {
+    console.error('Error setting session during login:', error);
+  }
+};
+
+export default setSessionOnLogin;
