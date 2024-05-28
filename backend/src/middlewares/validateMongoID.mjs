61d97961a288698c41759aac7568006ee9b331b1
Kevin Koech <kevinkoech@proton.me>
2024-05-28 16:11:49 +0300

    [Update]: Validate passed ID param middleware

---
+++ backend/src/middlewares/validateMongoID.mjs
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const validateMongoID = async (req, res, next) => {
+  // Get id from params
+  const { id } = req.params;
+  // Validate id
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  // Invalid id
+  if (!isValid) {
+    return res.status(400).json({ success: false, message: 'Invalid ID parameter.' });
+  }
+  // Id is valid
+  next();
+};
+
+export default validateMongoID;
