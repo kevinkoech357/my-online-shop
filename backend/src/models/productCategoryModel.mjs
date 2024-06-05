3a56339ff8f736c4ac711d5a0d548d2617c9adec
Kevin Koech <kevinkoech@proton.me>
2024-06-05 14:29:54 +0300

    [Update]: Define the ProductCategory model schema with title field

---
+++ backend/src/models/productCategoryModel.mjs
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+// Define Category model with only Title field
+
+const productCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true
+  }
+}, {
+  timestamps: true
+});
+
+const ProductCategory = mongoose.model('ProductCategory', productCategorySchema);
+
+export default ProductCategory;
