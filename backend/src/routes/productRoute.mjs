b04be9d68714b84908cc1c4ec27a33e060359fb7
Kevin Koech <kevinkoech@proton.me>
2024-06-06 10:35:29 +0300

    [Feat]: Add addToWishlist and getWishlist functions that enable authenticated users to update and view their wishlists Mount the specific routes for functions mentioned

--- backend/src/routes/productRoute.mjs
+++ backend/src/routes/productRoute.mjs
@@ -1,11 +1,20 @@
 import express from 'express';
-import { viewOneProduct, getAllProducts } from '../controller/productCtrl.mjs';
+import { viewOneProduct, getAllProducts, addToWishlist, getWishlist } from '../controller/productCtrl.mjs';
 import validateMongoID from '../middlewares/validateMongoID.mjs';
+import { checkRequiredFields } from '../middlewares/validateBody.mjs';
+import isAuthenticated from '../middlewares/userStatus.mjs';
 
 const productRouter = express.Router();
+
+// Const wishlist body
+const wishlistField = ['productID'];
 
 // Product related open/public routes
 productRouter.get('/', getAllProducts);
 productRouter.get('/:id', validateMongoID, viewOneProduct);
+
+// Wishlist related private routes
+productRouter.get('/wishlist/me', isAuthenticated, getWishlist);
+productRouter.put('/wishlist', checkRequiredFields(wishlistField), isAuthenticated, addToWishlist);
 
 export default productRouter;
