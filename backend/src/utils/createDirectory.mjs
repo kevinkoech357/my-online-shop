fc30673ffc3f12464f144754355c2333d6c013ca
Kevin Koech <kevinkoech@proton.me>
2024-06-10 21:07:23 +0300

    [Feat]: Add a check to validate price and quantity data types before creating product Asynchronously create directories using fs module

---
+++ backend/src/utils/createDirectory.mjs
@@ -0,0 +1,12 @@
+import fs from 'fs/promises';
+
+// Create the directories asynchronously if they don't exist
+const createDirectory = async (dir) => {
+  try {
+    await fs.mkdir(dir, { recursive: true });
+  } catch (error) {
+    console.error(`Error creating directory ${dir}: ${error}`);
+  }
+};
+
+export default createDirectory;
