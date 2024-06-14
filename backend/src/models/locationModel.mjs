413e3bd22135e3532cb525c4cde2674ff9eff8eb
Kevin Koech <kevinkoech@proton.me>
2024-06-14 22:04:31 +0300

    [Update]: Add modelling schema for Pickup locations

---
+++ backend/src/models/locationModel.mjs
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+// Define Location model with only Title field
+
+const locationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true
+  },
+  county: {
+    type: String,
+    required: true
+  },
+  town: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Location = mongoose.model('Location', locationSchema);
+
+export default Location;
