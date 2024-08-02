08d6da1ce54d165103a3c8eb613e46fbc74e2b32
Kevin Koech <kevinkoech@proton.me>
2024-08-02 22:59:08 +0300

    [Update]: Add Dark Mode, Lato font for heading and body

---
+++ frontend/src/components/ColorModeSwitch.jsx
@@ -0,0 +1,23 @@
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const ColorModeSwitch = () => {
+	const { toggleColorMode } = useColorMode();
+	const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+	return (
+		<IconButton
+			aria-label="Toggle color mode"
+			icon={<SwitchIcon />}
+			onClick={toggleColorMode}
+			variant="ghost"
+			color="current"
+			size="md"
+			margin={1}
+			padding={1}
+		/>
+	);
+};
+
+export default ColorModeSwitch;
